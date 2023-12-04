def1a37ec09adf7d03e77b8f16d23388dadbb491
allsilver11 <allsilver11@naver.com>
2023-12-04 23:04:32 +0900

    test

---
+++ src/test/java/com/jde/hanghae/controller/HealthCheckControllerTest.java
@@ -0,0 +1,14 @@
+package com.jde.hanghae.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HealthCheckControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
